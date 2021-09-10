8194f0487f83f11a803603f09ad795370c2a7f1d
Vijayasai Sanagaram <vijayasai10@gmail.com>
2021-09-10 23:17:36 +0530

    docker-entrypoint modified

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,13 +11,13 @@ echo "Migrate the Database at startup of project"
 
 # Wait for few minute and run db make migration
 while ! python manage.py makemigrations user 2>&1; do
-    echo "Migration is in progress status"
+    echo "Make Migration for specific app user"
     sleep 3
 done
 
 # Wait for few minute and run db migraiton
 while ! python manage.py migrate user 2>&1; do
-    echo "Migration is in progress status"
+    echo "Migration is in progress for user"
     sleep 3
 done
 
